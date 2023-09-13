8b2ca4ec76812e580dffa9fd5f49942c8662a6b1
Bilel <docka2000-2@yahoo.fr>
2023-09-13 17:14:28 +0200

    enabling HTTPS

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-
+import fs from 'fs'
 import express from 'express'
 import getConfig from "./utils/config";
 import cors from "cors";
+import https from "https";
 import { fetchActionsStart, notifyActionsRefresher } from './services/actions-fetcher';
 import { notifyDataRefresher, startDataRefreshTimer } from './services/data-refresher';
 import { dbClient, initializeDatabase } from './utils/database';
@@ -31,7 +32,12 @@ export const triggerDBinitialize = () => {
   });
 }
 
-app.listen(config.port, "0.0.0.0", () => {
-  console.log(`Server listening on port ${config.port}`);
-  triggerDBinitialize();
+https
+  .createServer({
+    key: fs.readFileSync("/home/ubuntu/key.pem"),
+    cert: fs.readFileSync("/home/ubuntu/cert.pem"),
+  }, app)
+  .listen(config.port, "0.0.0.0", () => {
+    console.log(`Server listening on port ${config.port}`);
+    triggerDBinitialize();
 })
