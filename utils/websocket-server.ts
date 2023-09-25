9e3941d74d51cfaa4a749424cca1d63bae9396fb
Bilel <docka2000-2@yahoo.fr>
2023-09-25 10:17:26 +0200

    fix: broadcast ws

--- utils/websocket-server.ts
+++ utils/websocket-server.ts
@@ -3,6 +3,12 @@ import { initializeWebSocketHandler } from '../services/data-refresher';
 
 export function initializeWebSocketServer(wsServer: WebSocketServer) {
     wsServer.on('connection', function connection(ws) {
-        initializeWebSocketHandler((message) => ws.send(message))
+        initializeWebSocketHandler((message) => {
+            wsServer.clients.forEach(function each(client) {
+                if (client.readyState === WebSocket.OPEN) {
+                  client.send(message);
+                }
+              });
+        })
     });
 }
